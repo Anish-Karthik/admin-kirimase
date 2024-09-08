036fc9f47c75ffcc81a7d24e68108ca2fc72dc7b
Anish-Karthik <anishkarthik.54321@gmail.com>
2024-09-08 10:46:02 +0530

    added TRPC

--- src/app/(app)/layout.tsx
+++ src/app/(app)/layout.tsx
@@ -1,9 +1,13 @@
 import { checkAuth } from "@/lib/auth/utils";
+import TrpcProvider from "@/lib/trpc/Provider";
+import { cookies } from "next/headers";
 export default async function AppLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
   await checkAuth();
-  return ( <main>{children}</main> )
+  return ( <main>
+<TrpcProvider cookies={cookies().toString()}>{children}</TrpcProvider>
+</main> )
 }
